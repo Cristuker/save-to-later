a20c85dc852a57fd1ed118416926cb7d2e441c01
Cristian <104389276+cris-ms@users.noreply.github.com>
2024-09-06 11:13:23 -0300

    refactor: session management

--- src/config/agentProxy.ts
+++ src/config/agentProxy.ts
@@ -8,38 +8,45 @@ export const generateAgentAndBot = async (): Promise<[AtpAgent, any]> => {
 
   const agent = new AtpAgent({
     service: "https://bsky.social",
-    persistSession: async (evt: string, session: AtpSessionData | undefined) => {
-      await saveSession(JSON.stringify(session));
-      console.log('Persist session');
-    }
+    persistSession: async (
+      evt: string,
+      session: AtpSessionData | undefined
+    ) => {
+      if (session) {
+        await saveSession(JSON.stringify(session));
+        console.log("Persist session");
+      }
+    },
   });
 
   const session = await getSession();
 
   if (session) {
-    console.log("Resume session");
-    await agent.resumeSession(JSON.parse(session));
-    await bot.resumeSession(JSON.parse(session))
-    return [agent, bot];
+    try {
+      await agent.resumeSession(JSON.parse(session));
+      await bot.resumeSession(JSON.parse(session));
+      return [agent, bot];
+    } catch (error) {
+      const { data } = await agent.login({
+        identifier: String(process.env.IDENTIFIER),
+        password: String(process.env.PASSWORD),
+      });
+      await bot.resumeSession({
+        refreshJwt: data.refreshJwt,
+        accessJwt: data.accessJwt,
+        handle: data.handle,
+        did: data.did,
+        email: data.email,
+        emailConfirmed: data.emailConfirmed,
+        emailAuthFactor: data.emailAuthFactor,
+        active: !!data.active,
+        status: data.status,
+      });
+      console.log("Create a new Session");
+    }
   }
 
-  const  { data } = await agent.login({
-    identifier: String(process.env.IDENTIFIER),
-    password: String(process.env.PASSWORD),
-  });
-  await bot.resumeSession({
-    refreshJwt: data.refreshJwt,
-    accessJwt: data.accessJwt,
-    handle: data.handle,
-    did: data.did,
-    email: data.email,
-    emailConfirmed: data.emailConfirmed,
-    emailAuthFactor: data.emailAuthFactor,
-    active: !!data.active,
-    status: data.status
-  })
 
-  console.log("Create a new Session");
 
   return [agent, bot];
 };
