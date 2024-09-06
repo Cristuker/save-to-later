bf799bc4558550e1bf5b97b1b1fdcee7e712b8ac
Cristian <104389276+cris-ms@users.noreply.github.com>
2024-09-06 12:21:09 -0300

    fix: add port

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,21 @@
 import { connectRedis, saveWrongMessage } from "./redis";
 import { generateAgentAndBot } from "./config/agentProxy";
 import { processMention } from "./mentions";
+import http from "http";
 import "dotenv/config";
 
 connectRedis();
 
+const PORT = 8080; 
+function handleRequest(request, response) {
+  console.log(request.headers);
+  response.end("It Works!! Path Hit: " + request.url);
+}
+
+var server = http.createServer(handleRequest);
+server.listen(PORT, function () {
+  console.log("Server listening on:", PORT);
+});
 
 export async function main() {
   const [agent, bot] = await generateAgentAndBot();
