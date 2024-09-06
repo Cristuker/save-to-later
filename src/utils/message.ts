bda620e16f213c34a98b210eb184c1bc12a6b7be
Cristuker <cristian123105@gmail.com>
2024-09-06 17:08:43 -0300

    refactor: cleaning message

--- src/utils/message.ts
+++ src/utils/message.ts
@@ -3,12 +3,12 @@ import AtpAgent, { RichText } from "@atproto/api"
 export const messageBuilder = async (url: string, message: string, agent: AtpAgent): Promise<RichText> => {
     const date = new Intl.DateTimeFormat(['pt-BR'], {
         dateStyle: 'short',
+        hour: 'numeric',
+        minute: 'numeric',
     }).format(new Date());
-    console.log(message)
     const messageWithoutMention = message.replace('@savetoread.bsky.social', '🔖')
-    console.log(messageWithoutMention)
     const rt = new RichText({
-        text: `[Saved at ${date}]: ${messageWithoutMention} - ${url}`,
+        text: `[${date}]: ${messageWithoutMention} - ${url}`,
     });
     await rt.detectFacets(agent)
 
