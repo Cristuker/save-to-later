dd52ee3a85861cf495c6a164eee8fd35f4968078
Cristuker <cristian123105@gmail.com>
2024-09-06 17:15:42 -0300

    fix: o bug que acabei de subir pra ter o que fazer

--- src/utils/message.ts
+++ src/utils/message.ts
@@ -3,8 +3,7 @@ import AtpAgent, { RichText } from "@atproto/api"
 export const messageBuilder = async (url: string, message: string, agent: AtpAgent): Promise<RichText> => {
     const date = new Intl.DateTimeFormat(['pt-BR'], {
         dateStyle: 'short',
-        hour: 'numeric',
-        minute: 'numeric',
+        timeStyle: 'short',
     }).format(new Date());
     const messageWithoutMention = message.replace('@savetoread.bsky.social', '🔖')
     const rt = new RichText({
