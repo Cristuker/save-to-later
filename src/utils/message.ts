6ab5cea169f89da22144c5d0b6be6e08c7672e60
Cristuker <cristian123105@gmail.com>
2024-09-10 15:51:15 -0300

    refactor: add timezone

--- src/utils/message.ts
+++ src/utils/message.ts
@@ -8,6 +8,7 @@ export const messageBuilder = async (
   const date = new Intl.DateTimeFormat(["pt-BR"], {
     dateStyle: "short",
     timeStyle: "short",
+    timeZone: "America/Sao_Paulo",
   }).format(new Date());
   const messageWithoutMention = message.replace(
     "@savetoread.bsky.social",
